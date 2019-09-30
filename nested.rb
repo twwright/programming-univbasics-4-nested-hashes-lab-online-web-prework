069ee1c58356c68805ac14a96b612627fec953bf
Thomas Wright <twwright@gmail.com>
2019-09-30 05:57:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,12 +60,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -82,7 +76,10 @@ def adding_matz
         }
      }
 
-
+		 programmer_hash[:yukihiro_matsumoto => {
+	 		:known_for => "Ruby",
+	 		:languages => ["LISP", "C"]
+	  }]
 end
 
 def changing_alan
