bb7d2104767fbed5a01095bdff7d871506a5e0aa
Thomas Wright <twwright@gmail.com>
2019-09-30 05:47:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+	progammer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
