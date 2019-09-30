9f7b589e15a133c6d124c8f63202238e6a3a6f96
Thomas Wright <twwright@gmail.com>
2019-09-30 06:08:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,9 +76,12 @@ def adding_matz
         }
      }
 
- 	programmer_hash[:yukihiro_matsumoto] = {
-		:known_for => "Ruby",
-		:languages => ["LISP", "C"]
+	yukihiro = {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+	}
+
+ 	programmer_hash[:yukihiro_matsumoto] = yukihiro
 	}
 end
 
